972736ddfaf8cc0ebd719db598f8bca5e1736670
Marlon Wen <marlonwen10@outlook.com>
2026-02-20 00:18:51 +0000

    Update AssemblyCompiler.kt

--- AssemblyCompiler.kt
+++ AssemblyCompiler.kt
@@ -1,10 +1,9 @@
-import kotlin.random.Random
-
-// ONLY 3 MODIFICATIONS YOU SHOULD MAKE TO THIS PROGRAM
+// ONLY 4 MODIFICATIONS YOU SHOULD MAKE TO THIS PROGRAM
 // THE "val program", where you write ur program (its in the middle of the file somewhere)
-// The "val tests" which is how many randomized tests to run (its in the bottom of the file somewhere)
-// And this one
-var detailedTests = false // Set true for detailed individual tests, and false if you are running a ton of tests
+// And these ones
+var detailedTests = true // Set true for detailed individual tests, and false if you are running a ton of tests
+var ultraDetailedTests = true // Tracer. Prints values every line. Probably only run one test
+val numberTests = 1 // Max is about 10 million if your cycle count is low.
 
 // The test cases are 2 randomly generated 16 bit numbers
 
@@ -12,65 +11,101 @@ var bigCycles = 0
 var success = true
 var bestCase = 1999999
 var worstCase = 0
+var fails = 0
 
 fun test() {
-    val r = MutableList(16) { 0 }
+    val r = MutableList(16) { -9999 }
     var cycles = 0
     var pc = 0
 
     fun add(d: Int, a: Int, b: Int): () -> Unit = {
-        r[d] = r[a] + r[b]
-        cycles++
-        pc++
-    }
+            if (ultraDetailedTests) {
+                println(" ${pc+1}: r[$d] = r[$a] + r[$b] = ${r[a]} + ${r[b]} = ${r[a] + r[b]}")
+            }
+            r[d] = r[a] + r[b]
+            cycles++
+            pc++
+        }
 
     fun sub(d: Int, a: Int, b: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            println(" ${pc+1}: r[$d] = r[$a] - r[$b] = ${r[a]} - ${r[b]} = ${r[a] - r[b]}")
+        }
         r[d] = r[a] - r[b]
         cycles++
         pc++
     }
 
     fun mul(d: Int, a: Int, b: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            println(" ${pc+1}: r[$d] = r[$a] * r[$b] = ${r[a]} * ${r[b]} = ${r[a] * r[b]}")
+        }
         r[d] = r[a] * r[b]
         cycles+=3
         pc++
     }
 
     fun shl(d: Int, a: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            println(" ${pc+1}: r[$d] = r[$d] shl r[$a] = ${r[d]} shl ${r[a]} = ${r[d] shl r[a]}")
+        }
         r[d] = r[d] shl r[a]
         cycles++
         pc++
     }
 
     fun shr(d: Int, a: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            println(" ${pc+1}: r[$d] = r[$d] shr r[$a] = ${r[d]} shr ${r[a]} = ${r[d] shr r[a]}")
+        }
         r[d] = r[d] shr r[a]
         cycles++
         pc++
     }
 
     fun bor(d: Int, a: Int, b: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            println(" ${pc+1}: r[$d] = r[$a] or r[$b] = ${r[a]} or ${r[b]} = ${r[a] or r[b]}")
+        }
         r[d] = r[a] or r[b]
         cycles++
         pc++
     }
 
     fun band(d: Int, a: Int, b: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            println(" ${pc+1}: r[$d] = r[$a] and r[$b] = ${r[a]} and ${r[b]} = ${r[a] and r[b]}")
+        }
         r[d] = r[a] and r[b]
         cycles++
         pc++
     }
 
     fun li(d: Int, i: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            println(" ${pc+1}: r[$d] = $i")
+        }
         r[d] = i
         cycles++
         pc++
     }
 
     fun jz(a: Int, offset: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            val willJump = r[a] == 0
+            val nextLine = if (willJump) pc + offset + 1 else pc + 2
+            println(" ${pc+1}: jz r[$a], $offset -> r[$a] == 0 is $willJump, next line will be $nextLine")
+        }
         if (r[a] == 0) pc += offset else pc++
         cycles+=3
     }
+    
     fun jp(a: Int, offset: Int): () -> Unit = {
+        if (ultraDetailedTests) {
+            val willJump = r[a] > 0
+            val nextLine = if (willJump) pc + offset + 1 else pc + 2
+            println(" ${pc+1}: jp r[$a], $offset -> r[$a] > 0 is $willJump, next line will be $nextLine")
+        }
         if (r[a] > 0) pc += offset else pc++
         cycles+=3
     }
@@ -81,23 +116,31 @@ fun test() {
 
     val program: List<() -> Unit> = listOf(
         // WRITE PROGRAM HERE
-       
+
+        // Example program, in function form with COMMAS at end     
+        //li(15, 0), 
+        //li(2, 0), 
+        //sub(14, 1, 0), 
+        //jp(14, 16), 
         
         // STORE QUOTIENT IN REGISTER 2 AS IN SPEC
         // STORE REMAINDER IN REGISTER 3 AS IN SPEC
     )
 
     val dividend = randomU16()
-    val divisor = randomU16()
+    var divisor = randomU16()
+    while (divisor == 0) {
+        divisor = randomU16()
+    }
 
-    r[0] = dividend
-    r[1] = divisor
+    r[0] = 60000
+    r[1] = 14000
 
     while (pc in program.indices) {
         program[pc]() 
 
-        if (cycles > 100000) {
-            println("⚠️ Infinite Loop Detected! PC: $pc")
+        if (cycles > 1000) {
+            println("⚠️ Infinite Loop Detected! PC: $pc, dividend: $dividend, divisor: $divisor")
             break
         }
     }
@@ -114,12 +157,16 @@ fun test() {
         println("Cycles = ${cycles}")
         println()
     }
-
-    success = success && (r[2] == (dividend / divisor) && r[3] == (dividend % divisor))
+	
+    if (r[2] != (dividend / divisor) || r[3] != (dividend % divisor)) {
+        success = false
+        fails += 1
+        println("Failed for: dividend = $dividend, divisor = $divisor")
+    }
 }
 
 fun main() {
-    val tests = 5000000
+    val tests = numberTests
 
     repeat (tests) {
         test()
@@ -128,4 +175,5 @@ fun main() {
     println("All $tests tests passed = $success")
     println("Worst case = $worstCase")
     println("Best case = $bestCase")
+    println("Fails = $fails")
 }
